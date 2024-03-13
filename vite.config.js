44bc5f20027a7554a14dc4c470e77ac5a7164fb9
Zach G <zabros20@gmail.com>
2024-03-13 09:45:57 -0600

    stopping ssr

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.js',
-            ssr: 'resources/js/ssr.js',
+            // ssr: 'resources/js/ssr.js',
             refresh: true,
         }),
         vue({
